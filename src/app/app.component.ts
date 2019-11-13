c283bc4409d6cae85c931f5be36ffd3372de6612
Lesly <p.sarahi@unitec.edu>
2019-11-12 22:50:06 -0600

    add json cities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import cities from 'cities.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'BonnieClyde-SimpleWeather';
+
+  ngOnInit() {
+
+    console.log(cities);
+
+  }
+
 }
